4cd1cae5ae0f853019f777870b3923bb299bc6db
Derick Dsilva <mstrfpts@gmail.com>
2020-09-28 17:22:37 +0100

    Trello with drag and drop for movment. Not ideal. Category drag enter issues.

--- src/Board.js
+++ src/Board.js
@@ -2,45 +2,58 @@ import React, { useState, useRef } from "react";
 import AddTaskModal from "./Modal";
 import { useLocalStorage } from "./CustomHooks";
 import TaskCards from "./TaskCards";
+import Categories from "./Categories";
 import "./Board.css";
 
 const Board = () => {
   const [showModal, setShowModal] = useState(false);
-
+  const staticElements = [
+    { title: "Drop Here", description: " ", id: 1, category: "ToDo" },
+    { title: "Drop Here", description: " ", id: 2, category: "Ongoing" },
+    { title: "Drop Here", description: " ", id: 3, category: "Complete" },
+  ];
   const [taskList, setTaskList] = useLocalStorage("taskList", [
+    ...staticElements,
     {
-      title: "Sample Task one",
-      description: "Sample Task one Description",
-      id: 1,
+      title: "Sample Task one ToDo",
+      description: "Sample Task one Description ToDo",
+      id: 4,
       category: "ToDo",
     },
     {
-      title: "Sample Task one",
-      description: "Sample Task one Description",
-      id: 1,
+      title: "Sample Task one Ongoing",
+      description: "Sample Task one Description Ongoing",
+      id: 5,
       category: "Ongoing",
     },
     {
-      title: "Sample Task one",
-      description: "Sample Task one Description",
-      id: 1,
+      title: "Sample Task one Complete",
+      description: "Sample Task one Description Complete",
+      id: 6,
       category: "Complete",
     },
   ]);
 
-  const categoryList = [
-    { name: "ToDo", id: 1 },
-    { name: "Ongoing", id: 2 },
-    { name: "Complete", id: 3 },
+  //Functions
+  const findCategoryTasks = (category) => {
+    let filteredTasks = taskList.filter((task) => task.category === category);
+    return filteredTasks.map((filteredTask) => filteredTask.id);
+  };
+
+  let categoryList = [
+    { name: "ToDo", id: 1, tasks: findCategoryTasks("ToDo") },
+    { name: "Ongoing", id: 2, tasks: findCategoryTasks("Ongoing") },
+    { name: "Complete", id: 3, tasks: findCategoryTasks("Complete") },
   ];
 
-  //Functions
   const findFreeId = (array) => {
     const sortedArray = array.slice().sort(function (a, b) {
       return a.id - b.id;
     });
+    console.log("derd filter sorted", sortedArray);
     let previousId = 0;
     for (let element of sortedArray) {
+      console.log("derd sorted array elemnet", element);
       if (element.id !== previousId + 1) {
         return previousId + 1;
       }
@@ -52,6 +65,18 @@ const Board = () => {
   const addTask = (task) => {
     let newTask = { ...task, id: findFreeId(taskList) };
     setTaskList([...taskList, newTask]);
+  };
+
+  const updateTask = (taskId, targetCategory) => {
+    let newTaskList = taskList.map((task, index) => {
+      if (task.id === taskId) {
+        console.log("derd, found taskId", task);
+        task.category = targetCategory;
+      }
+      return task;
+    });
+    console.log("derd updated new task", newTaskList);
+    setTaskList(newTaskList);
   };
 
   const filterCategoryTasks = (category) => {
@@ -74,16 +99,22 @@ const Board = () => {
   };
 
   return (
-    <div className={"Board"}>
-      {categoryList.map((category) => (
+    <div>
+      {/*categoryList.map((category) => (
         <CategoryCard key={category.id} title={category.name} />
-      ))}
-      <AddTaskModal
+      ))*/}
+      <Categories
+        addTask={addTask}
+        updateTask={updateTask}
+        categoryList={categoryList}
+        taskList={taskList}
+      />
+      {/*<AddTaskModal
         addTask={addTask}
         showModal={showModal}
         setShowModal={setShowModal}
         categoryList={categoryList}
-      />
+      />*/}
     </div>
   );
 };
---
+++ src/Categories.js
@@ -0,0 +1,139 @@
+import React, { useState, useRef, useEffect } from "react";
+import AddTaskModal from "./Modal";
+
+import "./Board.css";
+
+const Categories = ({ categoryList, taskList, addTask, updateTask }) => {
+  const [showModal, setShowModal] = useState(false);
+  const dragItem = useRef();
+  const dragNode = useRef();
+
+  const [dragging, setDragging] = useState(false);
+
+  useEffect(() => {
+    console.log("derd useEffect dragItem update", dragItem);
+  }, [dragItem]);
+
+  const filterCategoryTasks = (category) => {
+    return taskList.filter((task) => {
+      return task.category === category;
+    });
+  };
+
+  const dragStartHandler = (e, params) => {
+    console.log("drag start e", e.target);
+    console.log("drag start task", params);
+    //console.log("drag start task", task.title);
+    //console.log("drag start task", task.category);
+    dragItem.current = params;
+    dragNode.current = e.target;
+    dragNode.current.addEventListener("dragend", dragEndHandler);
+    setTimeout(() => setDragging(true), 0);
+    console.log("drag start dragItem", dragItem.current);
+  };
+
+  const dragEndHandler = () => {
+    console.log("derd, drag ending");
+    setDragging(false);
+
+    dragNode.current.removeEventListener("dragend", dragEndHandler);
+    dragItem.current = null;
+    dragNode.current = null;
+  };
+
+  const dragEnterHandler = (e, params) => {
+    //console.log("drag enter task", task.title);
+    //console.log("drag enter dragNode", dragNode.current);
+    // console.log("drag enter e", e.target);
+    // console.log("drag enter dragNode", dragNode.current);
+    console.log("drag enter dragitem category ", dragItem.current.category);
+    console.log("drag enter params.category", params.category);
+    console.log("drag enter e target", e.target);
+    console.log("drag enter dragNode.current", dragNode.current);
+
+    if (
+      dragItem.current.category !== params.category &&
+      e.target !== dragNode.current
+    ) {
+      console.log("derd drag enter not the same");
+      console.log("derd drag enter task id", dragItem.current.id);
+      console.log("derd drag enter target task category", params.category);
+      updateTask(dragItem.current.id, params.category);
+      //console.log("drag enter task", task.title);
+      //console.log("drag enter task", task.category);
+    }
+  };
+
+  const getStyle = (task) => {
+    const {
+      current: { category: dragItemCategory, title: dragItemTitle },
+    } = dragItem;
+    const { title: taskTitle, category: taskCategory } = task;
+    if (taskTitle === dragItemTitle && taskCategory === dragItemCategory) {
+      return "Dnd Task";
+    } else {
+      return "Task";
+    }
+  };
+
+  const CategoryCard = ({ title, setShowModal }) => {
+    let categoryTasks = categoryList.filter(
+      (category) => category.name === title
+    )[0].tasks.length;
+    return (
+      <div
+        className={"Category"}
+        onDragEnter={
+          dragging && !categoryTasks.length
+            ? (e) => dragEnterHandler(e, dragItem.current)
+            : null
+        }
+      >
+        <div className={"CategoryTitle"}>{title}</div>
+        {<TaskCards filteredTaskList={filterCategoryTasks(title)} />}
+        <button className={"AddTask"} onClick={() => setShowModal(true)}>
+          +
+        </button>
+      </div>
+    );
+  };
+
+  const TaskCards = ({ filteredTaskList }) => {
+    return filteredTaskList.map((task, index) => (
+      <div
+        draggable
+        onDragStart={
+          task.title !== "Tasks" ? (e) => dragStartHandler(e, task) : null
+        }
+        onDragEnter={dragging ? (e) => dragEnterHandler(e, task) : null}
+        key={index}
+        className={dragging ? getStyle(task) : "Task"}
+      >
+        <div className={"TaskTitle"}>{task.title}</div>
+        <div className={"TaskDesc"}>{`${
+          task.title !== "Drop Here" ? "-" : ""
+        } ${task.description}`}</div>
+      </div>
+    ));
+  };
+
+  return (
+    <div className={"Board"}>
+      {categoryList.map((category) => (
+        <CategoryCard
+          key={category.id}
+          title={category.name}
+          setShowModal={setShowModal}
+        />
+      ))}
+      <AddTaskModal
+        addTask={addTask}
+        showModal={showModal}
+        setShowModal={setShowModal}
+        categoryList={categoryList}
+      />
+    </div>
+  );
+};
+
+export default Categories;
