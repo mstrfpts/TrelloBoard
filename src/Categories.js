763ee0791f649746b959710f77523288716cadbb
Derick Dsilva <mstrfpts@gmail.com>
2020-10-04 14:14:32 +0100

    Fix for drag accross columns and change in task order within a column

--- src/Categories.js
+++ src/Categories.js
@@ -17,6 +17,7 @@ const Categories = ({
   const [taskToBeUpdated, setTaskToBeUpdated] = useState();
   const dragItem = useRef();
   const dragNode = useRef();
+  const [dragEnterId, setDragEnterId] = useState(-1);
 
   const [dragging, setDragging] = useState(false);
 
@@ -46,12 +47,12 @@ const Categories = ({
     dragNode.current.removeEventListener("dragend", dragEndHandler);
     dragItem.current = null;
     dragNode.current = null;
+    setDragEnterId(-1);
   };
 
   const dragEnterHandler = (e, params, source) => {
     if (source === "category") {
       if (params !== dragItem.current.category) {
-        console.log("derd, entered category update task", dragItem.current);
         updateTaskCategory(
           dragItem.current.id,
           dragItem.current.category,
@@ -59,10 +60,12 @@ const Categories = ({
         );
       }
     } else if (source === "task") {
-      if (params.id !== dragItem.current.id && e.target !== dragNode.current) {
-        console.log("task function call params target", params);
-        console.log("task function call params source", dragItem.current.id);
-        //updateTaskCategory(dragItem.current.id, params.category);
+      if (
+        dragEnterId !== params.id &&
+        params.id !== dragItem.current.id &&
+        e.target !== dragNode.current
+      ) {
+        setDragEnterId(params.id);
         updateTaskOrder(params.category, dragItem.current.id, params.id);
       }
     }
@@ -96,9 +99,6 @@ const Categories = ({
   };
 
   const CategoryCard = ({ title, setShowModal }) => {
-    /*let categoryTaskIds = categoryList.filter(
-      (category) => category.name === title
-    )[0].tasks;*/
     return (
       <div
         className={"Category"}
